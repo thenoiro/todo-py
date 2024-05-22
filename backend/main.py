65445db97e8db070107639ce182904507894491e
Bohdan Chernovol <noiro.bogdan@gmail.com>
2024-05-22 15:04:18 +0300

    update: database and basic connection

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine
+
+DB_USER = 'admin'
+DB_PASSWORD = '2024#admin'
+DB_NAME = 'todopy_db'
+DB_PORT = '5432'
+DB_HOST = 'localhost'
+
+DB_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_engine(DB_URL)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 @app.get('/test')
 def get_test():
